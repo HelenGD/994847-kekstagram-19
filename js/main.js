1611ee62feedceb5b813e8abc0887c68592f38e4
Elena Gontareva <e.s.gontareva@gmail.com>
2020-01-27 22:25:44 +0300

    Личный проект: больше деталей (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Генерирует случайное число от min до max
-var random = function (min, max) {
+var getRandomValue = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -23,7 +23,7 @@ var createPhotos = function () {
   for (var i = 0; i < 25; i++) {
     // Генерируем случайные комменты
     var photoComments = createComments();
-    photos.push(createPhoto(i + 1, 'desc', random(15, 200), photoComments));
+    photos.push(createPhoto(i + 1, 'Это моя фотография', getRandomValue(15, 200), photoComments));
   }
   return photos;
 };
@@ -41,7 +41,7 @@ var createComment = function (avatarId, message, name) {
 var createComments = function () {
   var names = ['Ваня', 'Петя', 'Коля', 'Семен', 'Игорь'];
   // Берем случайный индекс
-  var name = names[random(0, names.length - 1)];
+  var name = names[getRandomValue(0, names.length - 1)];
 
   var messages = [
     'Всё отлично!',
@@ -52,10 +52,10 @@ var createComments = function () {
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
   ];
 
-  var message = messages[random(0, messages.length - 1)];
+  var message = messages[getRandomValue(0, messages.length - 1)];
 
   var comments = [
-    createComment(random(1, 6), message, name)
+    createComment(getRandomValue(1, 6), message, name)
   ];
   return comments;
 };
@@ -95,6 +95,68 @@ var createPictures = function () {
   // Добавляем фрагмент со всеми фото на страницу
   picturesContainer.appendChild(fragment);
 };
-
 // Добавляет фотографии на страницу
 createPictures();
+
+// Отображение фотографии в полноэкранном режиме
+var photos = createPhotos();
+var bigPicture = document.querySelector('.big-picture');
+var currentPhoto = photos[0];
+var commentsBlock = bigPicture.querySelector('.social__comments');
+
+// Создает один комментарий на основе шаблона
+var makeСomment = function (comment) {
+  var commentEl = commentsBlock.querySelector('.social__comment')
+    .cloneNode(true);
+  var commentPicture = commentEl.querySelector('.social__picture');
+  var commentText = commentEl.querySelector('.social__text');
+
+  commentPicture.src = comment.avatar;
+  commentPicture.alt = comment.name;
+  commentText.textContent = comment.message;
+  return commentEl;
+};
+
+// Создает блок комментариев на основе шаблона
+var makeComments = function (comments) {
+  // Прячем блоки счётчика комментариев и загрузки новых комментариев
+  var commentsCount = bigPicture.querySelector('.social__comment-count');
+  var commentsLoader = bigPicture.querySelector('.comments-loader');
+  commentsCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < comments.length; i++) {
+    var itemComment = makeСomment(comments[i]);
+    fragment.appendChild(itemComment);
+  }
+  commentsBlock.contextText = '';
+  commentsBlock.appendChild(fragment);
+};
+
+// Показывает большую фотографию с лайками и коментариями
+var showBigPicture = function () {
+  var bigImage = bigPicture.querySelector('.big-picture__img');
+  var likesBigImage = bigPicture.querySelector('.likes-count');
+  var commentsBigImage = bigPicture.querySelector('.comments-count');
+  var captionBigImage = bigPicture.querySelector('.social__caption');
+  bigPicture.classList.remove('hidden');
+
+  bigImage.src = currentPhoto.url;
+  likesBigImage.textContent = currentPhoto.likes;
+  commentsBigImage.textContent = currentPhoto.comments.length;
+  captionBigImage.textContent = currentPhoto.description;
+
+  makeComments(currentPhoto.comments);
+};
+// Добавляет большую фотографию на страницу
+showBigPicture();
+
+// Добавляет body класс, чтобы контейнер с фотографиями позади не прокручивался при скролле
+var deleteScroll = function () {
+  var noScroll = document.querySelector('body');
+  noScroll.classList.add('modal-open');
+};
+// Убирает прокручивание при скролле
+deleteScroll();
+
