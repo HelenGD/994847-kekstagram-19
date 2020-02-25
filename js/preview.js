3901070df81d77a5b1bb9d920c47c39eb87f4861
Elena Gontareva <e.s.gontareva@gmail.com>
2020-02-26 00:55:04 +0300

    make task with comments

--- js/preview.js
+++ js/preview.js
@@ -2,6 +2,8 @@
 
 window.preview = (function () {
   var DISPLAY_COMMENTS_COUNT = 5;
+  var page = 0;
+  var currentComments = [];
   var bigPictureEl = document.querySelector('.big-picture');
   var bigImageEl = bigPictureEl.querySelector('.big-picture__img img');
   var likesBigImageEl = bigPictureEl.querySelector('.likes-count');
@@ -11,17 +13,29 @@ window.preview = (function () {
   var picturesContainerEl = document.querySelector('.pictures');
   var commentEl = document.querySelector('.social__footer-text');
   var commentsLoaderEl = bigPictureEl.querySelector('.comments-loader');
+  var commentsCountEl = bigPictureEl.querySelector('.social__comment-count');
 
   // Показывает большую фотографию с лайками и комментариями
   var showBigPicture = function (currentPhoto) {
-
+    page = 0;
+    currentComments = currentPhoto.comments;
     bigImageEl.src = currentPhoto.url;
     likesBigImageEl.textContent = currentPhoto.likes;
     commentsBigImageEl.textContent = currentPhoto.comments.length;
     captionBigImageEl.textContent = currentPhoto.description;
 
-    window.comment.render(currentPhoto.comments.slice(0, DISPLAY_COMMENTS_COUNT));
+    var commentsOnPage = getNextComments();
+    window.comment.render(commentsOnPage);
+
+    if (!isLastPage()) {
+      commentsLoaderEl.classList.remove('hidden');
+    }
+    setNumberCommentsString(commentsOnPage);
     openBigPicture();
+  };
+
+  var setNumberCommentsString = function (commentsOnPage) {
+    commentsCountEl.textContent = commentsOnPage.length + ' из ' + currentComments.length + ' комментариев';
   };
 
   var openBigPicture = function () {
@@ -55,11 +69,31 @@ window.preview = (function () {
     }
   });
 
+  var isLastPage = function () {
+    return currentComments.length <= DISPLAY_COMMENTS_COUNT * page;
+  };
+
+  var getNextComments = function () {
+    page += 1;
+    return currentComments.slice(0, DISPLAY_COMMENTS_COUNT * page);
+  };
+
   buttonEl.addEventListener('click', function () {
     closeBigPicture();
   });
 
   commentEl.addEventListener('keydown', onKeyDown);
+
+  commentsLoaderEl.addEventListener('click', function () {
+    var commentsOnPage = getNextComments();
+    window.comment.render(getNextComments());
+
+    if (isLastPage()) {
+      commentsLoaderEl.classList.add('hidden');
+    }
+
+    setNumberCommentsString(commentsOnPage);
+  });
 
   return {
     showBigPicture: showBigPicture,
